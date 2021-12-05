a1fcd3780a9509d558b217942b6c56ae4fcb14f5
George Claghorn <george.claghorn@gmail.com>
2021-12-05 13:44:13 -0500

    Guard against ApplicationGraph already being defined

--- lib/graphql-rails-namespace.rb
+++ lib/graphql-rails-namespace.rb
@@ -3,7 +3,7 @@ require "rails/railtie"
 class GraphQLRailsNamespace < Rails::Railtie
   config.graphql = ActiveSupport::OrderedOptions.new unless config.respond_to?(:graphql)
   config.graphql.path = "app/graphql"
-  config.graphql.namespace = ::ApplicationGraph = Module.new
+  config.graphql.namespace = ::ApplicationGraph ||= Module.new
 
   initializer :remove_graphql_path_from_default_eager_load_paths, before: :set_eager_load_paths do |app|
     app.config.eager_load_paths -= [ full_graphql_path ]
