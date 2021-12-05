f21ed5808916a1bc3e9da54412cf0fa37bac098a
George Claghorn <george.claghorn@gmail.com>
2021-12-05 13:46:43 -0500

    Don't define ApplicationGraph unless it's needed

--- lib/graphql-rails-namespace.rb
+++ lib/graphql-rails-namespace.rb
@@ -3,7 +3,7 @@ require "rails/railtie"
 class GraphQLRailsNamespace < Rails::Railtie
   config.graphql = ActiveSupport::OrderedOptions.new unless config.respond_to?(:graphql)
   config.graphql.path = "app/graphql"
-  config.graphql.namespace = ::ApplicationGraph ||= Module.new
+  config.graphql.namespace = -> { ::ApplicationGraph ||= Module.new }
 
   initializer :remove_graphql_path_from_default_eager_load_paths, before: :set_eager_load_paths do |app|
     app.config.eager_load_paths -= [ full_graphql_path ]
@@ -14,11 +14,15 @@ class GraphQLRailsNamespace < Rails::Railtie
   end
 
   initializer :autoload_from_graphql_path do
-    Rails.autoloaders.main.push_dir full_graphql_path, namespace: config.graphql.namespace
+    Rails.autoloaders.main.push_dir full_graphql_path, namespace: namespace
   end
 
   private
     def full_graphql_path
       "#{Rails.root}/#{config.graphql.path}"
+    end
+
+    def namespace
+      config.graphql.namespace.try(:call) || config.graphql.namespace
     end
 end
